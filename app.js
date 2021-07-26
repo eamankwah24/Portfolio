4dcc3fa2b4ce520bada6a7da99e48bf6314f8a58
Edmund Amankwah <edmund_amankwah@yahoo.com>
2021-07-26 11:22:35 -0400

    hosted website on server

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
-const _=require('lodash')
+const http=require('http')
+const {readFileSync}=require('fs');
 
-const items=[1,[2,[3,[4]]]]
+//get all files
+const homePage=readFileSync('./index.html')
+const homeStyle=readFileSync('./style.css')
+const homeImage=readFileSync('./images/IMG_E1067.JPG')
 
-const newItems=_.flattenDeep(items);
+const server=http.createServer((req,res)=>{
+  const url=req.url;
+    //returning home page
+  if (url==='/'){
+    res.writeHead(200,{'content-type':'text/html'})
+    res.write(homePage)
+    res.end()      
+  }
+  //returns styles sheet to browser
+  else if(url==='/style.css'){
+    res.writeHead(200,{'content-type':'text/css'})
+    res.write(homeStyle)
+    res.end() 
+  }
+  //returns image to browser
+  else if(url==='/images/IMG_E1067.JPG'){
+    res.writeHead(200,{'content-type':'image/jpg'})
+    res.write(homeImage)
+    res.end()
+  }
+  //404 if url is not found
+  else{
+    res.writeHead(404,{'content-type':'text/html'})
+    res.write('<h1>page not found</h1>')
+    res.end()
+  }
+})
 
-console.log(newItems);
+server.listen(5000)
